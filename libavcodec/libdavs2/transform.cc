58a52cda71d8be19c5881f6fbf653150b83a63fd
Jamaika1 <lukaszcz18@wp.pl>
2024-08-18 16:36:40 +0200

    Defined XOP,AVX,AVX2

--- libavcodec/libdavs2/transform.cc
+++ libavcodec/libdavs2/transform.cc
@@ -993,7 +993,7 @@ void davs2_dct_init(uint32_t cpuid, ao_funcs_t *fh)
         }
     }
 
-    /* TODO: ³õÊ¼»¯·ÇÄ¬ÈÏDCTÄ£°å */
+    /* TODO: åˆå§‹åŒ–éžé»˜è®¤DCTæ¨¡æ¿ */
     if (cpuid & DAVS2_CPU_SSE2) {
         /* square */
         fh->idct[PART_8x8  ][DCT_HALF] = idct_8x8_half_sse128;
@@ -1020,6 +1020,7 @@ void davs2_dct_init(uint32_t cpuid, ao_funcs_t *fh)
         fh->idct[PART_64x16][DCT_QUAD] = idct_64x16_quad_sse128;
     }
 
+#if defined(__AVX2__)
 #if ARCH_X86_64
     if (cpuid & DAVS2_CPU_AVX2) {
         fh->idct[PART_8x8  ][DCT_DEAULT]   = idct_8x8_avx2;
@@ -1027,7 +1028,7 @@ void davs2_dct_init(uint32_t cpuid, ao_funcs_t *fh)
         fh->idct[PART_64x64][DCT_DEAULT] = idct_64x64_avx2;
         fh->idct[PART_64x16][DCT_DEAULT] = idct_64x16_avx2;
         fh->idct[PART_16x64][DCT_DEAULT] = idct_16x64_avx2;
-        fh->idct[PART_32x32][DCT_DEAULT] = idct_32x32_avx2;    // @luofl i7-6700k ËÙ¶È±Èsse128¿ìÒ»±¶
+        fh->idct[PART_32x32][DCT_DEAULT] = idct_32x32_avx2;    // @luofl i7-6700k é€Ÿåº¦æ¯”sse128å¿«ä¸€å€
 
         /* square */
         // fh->idct[PART_8x8  ][DCT_HALF] = idct_8x8_half_avx2;
@@ -1054,5 +1055,6 @@ void davs2_dct_init(uint32_t cpuid, ao_funcs_t *fh)
         // fh->idct[PART_64x16][DCT_QUAD] = idct_64x16_quad_avx2;
     }
 #endif  // if ARCH_X86_X64
+#endif
 #endif  // if HAVE_MMX
 }
