c7f173386959eeb5321cf0877c733660743d91f0
Jamaika1 <lukaszcz18@wp.pl>
2024-08-23 09:13:21 +0200

    Initial uavs3d uavs3/uavs3d@1fd0491

---
+++ libavcodec/libuavs3d/version.h
@@ -0,0 +1,17 @@
+// ===========================================================================
+// version.h
+// - collection of version numbers
+// ===========================================================================
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define VER_MAJOR  1                // major version number
+#define VER_MINOR  2                // minor version number
+#define VER_BUILD  0                // build number
+
+#define VERSION_TYPE "1fd0491"
+#define VERSION_STR  "1.2.0"
+#define VERSION_SHA1 "1fd0491"
+
+#endif // __VERSION_H__
