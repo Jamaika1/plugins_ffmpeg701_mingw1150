427c16ea6bea059f5cd8439373faff86ec34b385
Jamaika1 <lukaszcz18@wp.pl>
2024-11-11 10:47:20 +0100

    Update pixman/pixman@8d7a2f8

--- libavcodec/librsvg/pixman/pixman-version.h
+++ libavcodec/librsvg/pixman/pixman-version.h
@@ -32,10 +32,10 @@
 #endif
 
 #define PIXMAN_VERSION_MAJOR 0
-#define PIXMAN_VERSION_MINOR 43
-#define PIXMAN_VERSION_MICRO 5
+#define PIXMAN_VERSION_MINOR 44
+#define PIXMAN_VERSION_MICRO 1
 
-#define PIXMAN_VERSION_STRING "0.43.5-b753a6f"
+#define PIXMAN_VERSION_STRING "0.44.1-0d74d39"
 
 #define PIXMAN_VERSION_ENCODE(major, minor, micro) (	\
 	  ((major) * 10000)				\
